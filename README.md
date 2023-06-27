8097246ae816e57d6da8c47a87bf93761ad9c318
mayuraitavadekar <mayuraitavadekar2690@gmail.com>
2023-06-27 17:45:49 -0400

    sending data from controller to view

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 [1] basic template for spring MVC project. Apache tomcat version 10 is required. Java 17+ is required. visit 
 http://localhost:8080/demospringmvcproject/home 
+
+[2] simple controllers are added to understand how to send data from controller to view.
--- src/main/java/demospringmvcproject/controller/HomeController.java
+++ src/main/java/demospringmvcproject/controller/HomeController.java
@@ -1,7 +1,9 @@
 package demospringmvcproject.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class HomeController {
@@ -11,5 +13,24 @@ public class HomeController {
 		System.out.println("this is home URL");
 		return "index";
 	}
+	
+	@RequestMapping("/about")
+	public String getApplicationName(Model model) {
+		// this end-point demonstrates use of Model object
+		// to send data from controller to view
+		model.addAttribute("applicationName", "demospringmvc project");
+		return "about";
+	}
+	
+	@RequestMapping("/help")
+	public ModelAndView getHelp() {
+		// this end-point demonstrates use of ModelAndView object
+		// to send data from controller to view
+		ModelAndView mv = new ModelAndView();
+		mv.addObject("phone", "9890186562");
+		mv.setViewName("help");
+		return mv;
+	}
+	
 	
 }
